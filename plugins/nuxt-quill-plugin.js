4a89784b1faa4eb81817b8780ceaa8f2fb6f3667
吴朝东 <576108100@qq.com>
2019-09-15 15:11:26 +0800

    '重新提交'

--- plugins/nuxt-quill-plugin.js
+++ plugins/nuxt-quill-plugin.js
@@ -1,9 +1,5 @@
+
 import Vue from 'vue'
 import VueQuillEditor from 'vue-quill-editor/dist/ssr'
 
-if (process.browser) {
-    const VueQuillEditor = require('vue-quill-editor/dist/ssr')
-    Vue.use(VueQuillEditor, /* { default global options } */)
-  }
-  
 Vue.use(VueQuillEditor)
